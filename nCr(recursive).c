61f77697ca14adb422bdde553467c970e6b288b1
Sarthak0714 <91636065+sarthak0714@users.noreply.github.com>
2022-06-04 19:06:04 +0530

    fixed code

--- nCr(recursive).c
+++ nCr(recursive).c
@@ -4,7 +4,8 @@ void main()
 	int n,r,c;
 	printf("Enter N and R: ");
 	scanf("%d%d",&n,&r);
-
+	if(n<r)
+		printf("N should be greater than R(ERROR)\n");
 	c = fact(n)/(fact(r)*fact(n-r));
 
 	printf("The value of  %dC%d is %d",n,r,c);
@@ -12,16 +13,8 @@ void main()
 }
 int fact(int n)
 {
-	int i,f=1;
-	for(i=1;i<=n;i++)
-	{
-	f=f*i;
-	}
-	if(f==0)
-	{printf("INVALID NUMBERS\n");}
+    if(n==0)
+    return 1;
 
-	else
-	return f;
-
-
+    return n*fact(n-1);
 }
